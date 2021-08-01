39bb53b679757152a788e3b68f62220b0d18a68a
PLAZMAMA <maorioriori@gmail.com>
2021-08-01 18:02:41 -0400

    init

---
+++ ChatNGameWebsite/user/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+
+class LoginViewSet(viewsets.ViewSet):
+    def list():
+        pass
+
+class RegisterViewSet(viewsets.ViewSet):
+    def list():
+        pass
