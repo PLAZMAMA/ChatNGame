445d7419e0a232d231e73f0e094ec902a09e0650
PLAZMAMA <maorioriori@gmail.com>
2021-08-01 18:33:33 -0400

    created the base of the login and register viewsets

--- ChatNGameWebsite/user/views.py
+++ ChatNGameWebsite/user/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from rest
 from rest_framework import viewsets
 
+#viewset that handles the login process of the website
 class LoginViewSet(viewsets.ViewSet):
-    def list():
+    def list(self, request):
         pass
 
+    def create(self, request):
+        pass
+
+#viewset that handles the register process of the website
 class RegisterViewSet(viewsets.ViewSet):
-    def list():
+    def list(self, request):
+        pass
+
+    def create(self, request):
         pass
