1976ad8bb3e590990ccd69947269d26638f58f1b
PLAZMAMA <maorioriori@gmail.com>
2021-08-01 18:32:39 -0400

    removed unnessecary import and fixed the 'user/' path

--- ChatNGameWebsite/ChatNGameWebsite/urls.py
+++ ChatNGameWebsite/ChatNGameWebsite/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import user
 
 
 urlpatterns = [
-    path('user/', include(user.urls)),
+    path('user/', include('user.urls')),
     path('admin/', admin.site.urls),
 ]
